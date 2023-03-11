e1012febca86acb65adff6e4f3011f755256b02d
rodricabrera98 <rodricabrera98@gmail.com>
2023-03-11 11:45:26 -0300

    upload examen parcial

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,115 @@
 package tickets
 
+import (
+	"fmt"
+	"os"
+	"encoding/csv"
+	"strings"
+	"log"
+	"strconv"
+)
+
 type Ticket struct {
+	id int
+	nombre string
+	email string
+	pais_destino string
+	hora_vuelo string
+	precio int
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+// Este metodo recibe por parametros un destino y nos indica cuantos tickets de vuelo hay vendidos hacia ese destination
+
+func GetTotalTickets(destination string) (int, error) {
+	aux := 0
+	file, err := os.Open("./tickets.csv")
+	if(err != nil){
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	allTickets, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//var tickets []Ticket
+	for _, ticket := range allTickets {
+		pais_destino := ticket[3]
+		if pais_destino == destination {
+			aux++
+		}
+	}
+	return aux, nil
+
+}
 
 // ejemplo 2
-func GetMornings(time string) (int error) {}
+// Este metodo recibe un momento del dia, puede ser (madruagada 0-6; mañana 7-12; tarde 13-19; noche 20-23)
+// nos permite saber en que momento del dia se realizan mas vuelos, siendo este actualmente en la "madrugada"
+func GetCountByPeriod(time string) (int, error) {
+	file, err := os.Open("./tickets.csv")
+	if(err != nil){
+		log.Fatalf("error al abrir el archivo de tickects.csv")
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	allTickets, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	aux := 0
+	for _, ticket := range allTickets {
+		hour := ticket[4]
+		splitHour := strings.Split(hour, ":")
+		intHour, err := strconv.Atoi(splitHour[0])
+		if err != nil{
+			log.Printf("error convirtiendo hora(string) a int: %s", err)
+			continue
+		}
+
+		switch {
+			case intHour <= 6:
+				if time == "madrugada" {
+					aux++
+				}
+			case  intHour <= 12:
+				if time == "mañana" {
+					aux++
+				}
+		
+			case intHour <= 19:
+				if time == "tarde" {
+					aux++
+				}
+		
+			case intHour <= 23:
+				if time == "noche" {
+					aux++
+				}
+		
+			default:
+				fmt.Printf("Unexpected hour value: %d\n", intHour)
+	}
+}
+	return aux, nil
+}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+// Este metodo recibira un destination, que puede ser cualquier pais a donde volemos 
+// y un total de tickets cargados en la BD. Actualmente 11/03/23 poseemos 1000 tickets de vuelo
+// este metodo nos permite saber que paises reciben mas % de vuelos.
+
+func AverageDestination(destination string, total int) (float64, error) {
+	allTickets, err := GetTotalTickets(destination)
+	if err != nil {
+		return 0, err
+	}
+	promedio := (float64(allTickets)/float64(total))
+	return promedio, err
+}
--- main.go
+++ main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+	"fmt"
+	"log"
 )
+const (
+	destination = "Canada"
+)
+func getTotalTickets() {
+	total, err := tickets.GetTotalTickets(destination)
+	if err != nil {
+		log.Printf("Fallo al obtener el total de tickets: %v", err)
+		return
+	}
+	fmt.Printf("Total de tickets vendido a %s: %d\n", destination, total)
+}
+
+
+func getCountByPeriod() {
+	count, err := tickets.GetCountByPeriod("noche")
+	if err != nil {
+		log.Printf("Fallo al obtener la cantidad de viajeros viajando en este periodo: %v", err)
+		return
+	}
+	fmt.Printf("Total de pasajeros viajando de noche: %d\n", count)
+}
+
+func getAverageDestination() {
+	const totalTickets = 1000
+	promedio, err := tickets.AverageDestination(destination, totalTickets)
+	if err != nil {
+		log.Printf("Fallo al obetener el promedio de viajes: %v", err)
+		return
+	}
+	fmt.Printf("Promedio de tickets a %s: %.2f\n", destination, promedio)
+}
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	getTotalTickets()
+	getCountByPeriod()
+	getAverageDestination()
 }
